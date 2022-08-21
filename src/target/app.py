3c237a330c9bb39eef3649d9891ded02cf0d3ce2
George Tian <github@georgetian.com>
2022-08-21 23:17:42 +0800

    test

---
+++ src/target/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route('/logo')
+def logo():
+    send_file('logo.png')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', '8880')
