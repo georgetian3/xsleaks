5dcaeaa5fa0b9fa39bd6ce2291fdec47fa1661bc
George Tian <github@georgetian.com>
2022-08-28 00:11:27 +0800

    range

--- src/range/target.py
+++ src/range/target.py
@@ -2,21 +2,9 @@ from flask import Flask, request, Response, make_response
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    try:
-        status = int(dict(request.args)['status'])
-    except:
-        status = 401
-    return 'test', status
-
-""" @app.after_request
-def headers(resp):
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    resp.headers['Cache-Control'] = 'no-store'
-    print(resp)
-    return resp """
+    return '', int(dict(request.args)['status'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8880)
