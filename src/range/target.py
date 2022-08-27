8ae8d24edf7e74f8bd0dd1cc754aea70dc3ceb47
George Tian <github@georgetian.com>
2022-08-28 00:05:24 +0800

    range

--- src/range/target.py
+++ src/range/target.py
@@ -13,12 +13,12 @@ def index():
     print('status', request.get_json())    
     return 'test', 123
 
-@app.after_request
+""" @app.after_request
 def headers(resp):
     resp.headers['Access-Control-Allow-Origin'] = '*'
     resp.headers['Cache-Control'] = 'no-store'
     print(resp)
-    return resp
+    return resp """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8880)
