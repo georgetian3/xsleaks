3a3e2909878b7ccd6bdf18337ee55d6f0a429234
George Tian <github@georgetian.com>
2022-09-02 16:28:22 +0800

    update

--- src/range/attacker.py
+++ src/range/attacker.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_file('static/attacker.html')
+    return send_file('attacker.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8880)
