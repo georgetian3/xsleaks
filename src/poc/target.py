0da8472851b5d145f293d06be09ceb42afecca16
George Tian <github@georgetian.com>
2022-09-02 16:47:56 +0800

    update

--- src/poc/target.py
+++ src/poc/target.py
@@ -2,9 +2,13 @@ from flask import Flask, request, Response, make_response
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/exists')
+def exists():
     return 'exists'
+
+@app.route('/does_not_exist')
+def does_not_exist():
+    return 'does not exist'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8880)
