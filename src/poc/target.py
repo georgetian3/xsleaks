e91baa6cc095ee3d5a4b6c27fba8b2d2376da93f
George Tian <github@georgetian.com>
2022-09-02 16:48:33 +0800

    update

--- src/poc/target.py
+++ src/poc/target.py
@@ -8,7 +8,7 @@ def exists():
 
 @app.route('/does_not_exist')
 def does_not_exist():
-    return 'does not exist'
+    return 'does not exist', 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8880)
