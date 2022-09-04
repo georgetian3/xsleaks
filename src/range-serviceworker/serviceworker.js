a954972a5f659683492b824238df507abdb01769
George Tian <github@georgetian.com>
2022-09-04 17:36:33 +0800

    update

--- src/range-serviceworker/serviceworker.js
+++ src/range-serviceworker/serviceworker.js
@@ -7,7 +7,7 @@ self.addEventListener("install", function() {
 self.addEventListener("fetch", function(e) {
   let url = new URL(e.request.url);
   console.log(url);
-  if (e.request.headers.get("range") != "bytes=0-") {
+  if (e.request.headers.get("range") === "bytes=0-") {
     console.log('if');
     let urlParams = new URLSearchParams(url.search);
     e.respondWith(
