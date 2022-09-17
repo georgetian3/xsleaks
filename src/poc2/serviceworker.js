093d43ea087216c18ac47bc32411ea00982d56f9
George Tian <github@georgetian.com>
2022-09-17 19:30:00 +0800

    update

--- src/poc2/serviceworker.js
+++ src/poc2/serviceworker.js
@@ -9,7 +9,6 @@ self.addEventListener("fetch", function(e) {
   let url = new URL(e.request.url);
   let urlParams = new URLSearchParams(url.search);
   let size = urlParams.get("size");
-  console.log(size);
   let body = "A".repeat(Number(size));
   if (e.request.headers.get("range") === "bytes=0-") {
   	e.respondWith(new Response(body, {status: 206, headers: {"Content-Range": `bytes 0-${size - 1}/100000` }}));
