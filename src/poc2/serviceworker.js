224953ea4a9916d750a8465a3fd8703afba1cac7
George Tian <github@georgetian.com>
2022-09-17 19:31:40 +0800

    update

--- src/poc2/serviceworker.js
+++ src/poc2/serviceworker.js
@@ -1,16 +1,17 @@
 'use strict';
 
-self.addEventListener("install", function() {
+self.addEventListener('install', function() {
     self.skipWaiting();
 });
 
-self.addEventListener("fetch", function(e) {
+self.addEventListener('fetch', function(e) {
   let url = new URL(e.request.url);
   let urlParams = new URLSearchParams(url.search);
-  let size = urlParams.get("size");
-  let body = "A".repeat(Number(size));
-  if (e.request.headers.get("range") === "bytes=0-") {
+  let size = urlParams.get('size');
+  let intercept = urlParams.get('intercept');
+  let body = 'A'.repeat(Number(size));
+  if (intercept == 'true') {
     console.log('intercepted');
-  	e.respondWith(new Response(body, {status: 206, headers: {"Content-Range": `bytes 0-${size - 1}/100000` }}));
+  	e.respondWith(new Response(body, {status: 206, headers: {'Content-Range': `bytes 0-${size - 1}/100000` }}));
   }
 });
